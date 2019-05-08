39c2c72f13e209b7085d59abccefa822663439b6
Connor <callaksk@gmail.com>
2019-05-08 17:41:21 -0400

    Final update

--- resume2/app.js
+++ resume2/app.js
@@ -1,10 +1,10 @@
 $(() => {
 
-  $('#alert').toggleClass('hide')
-
   setTimeout(() => {
-    $('#alert').toggleClass('hide')
-  }, 3000)
+    $('body').append("<div id='alert'><p>If you want to download as a PDF file, please press the key '1'.</p></div>")
+
+      setTimeout(() => $('#alert').remove(), 3000)
+  }, 1500)
 
   $(document).keyup(e => {
     if(e.which == 49) window.open('https://v2018.api2pdf.com/chrome/url?url=https://wisdomkim.github.io/resume2/&apikey=765b92fa-975e-48f5-86e5-715419e9369a')
