865fad761cc43da482a693b4165f2e2c0423533e
Connor <callaksk@gmail.com>
2019-05-08 17:02:53 -0400

    PDF button

---
+++ resume2/app.js
@@ -0,0 +1,5 @@
+$(() => {
+  $("#pdf").click(() => {
+    window.open('https://v2018.api2pdf.com/chrome/url?url=https://wisdomkim.github.io/resume2/&apikey=765b92fa-975e-48f5-86e5-715419e9369a')
+  })
+})
