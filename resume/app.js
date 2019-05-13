08a77195228c65bcb45813b65e8ab4a0a100249f
Connor <callaksk@gmail.com>
2019-05-13 15:29:00 -0400

    2nd Update on May 13th, 2019

--- resume/app.js
+++ resume/app.js
@@ -7,6 +7,6 @@ $(() => {
   }, 1500)
 
   $(document).keyup(e => {
-    if(e.which == 49) window.open('https://v2018.api2pdf.com/chrome/url?url=https://wisdomkim.github.io/resume2/&apikey=765b92fa-975e-48f5-86e5-715419e9369a')
+    if(e.which == 49) window.open('https://v2018.api2pdf.com/chrome/url?url=https://wisdomkim.github.io/resume/&apikey=765b92fa-975e-48f5-86e5-715419e9369a')
   })
 })
